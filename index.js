1a4e4372fe4a7ce5f249ec7d24582c85d155ceb2
Ashishyemul <yemulashish@gmail.com>
2023-10-17 01:53:36 +0530

    final

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ class Block {
     constructor(xAxis, yAxis){
         this.bottomLeft = [xAxis, yAxis]
         this.bottomRight = [xAxis + blockWidth, yAxis]
-        this.topLeft = [xAxis, yAxis + blockHeight]
         this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+        this.topLeft = [xAxis, yAxis + blockHeight]
+       
     }
 }
 
@@ -62,19 +63,6 @@ console.log(blocks[i].bottomLeft);
 }
 addBlocks()
 
-
-
-// draw the ball
-function drawBall(){
-    ball.style.left = ballCurrentPosition[0] + 'px'
- ball.style.bottom = ballCurrentPosition[1] + 'px'
-}
-
-
-
- document.addEventListener('keydown', moveUser)
-
-
  //add user
  const user = document.createElement('div')
  user.classList.add('user')
@@ -85,8 +73,8 @@ function drawBall(){
  // add ball 
  const ball = document.createElement('div')
  ball.classList.add('ball')
- drawBall()
  grid.appendChild(ball)  
+ drawBall()
 
 
  //move user
@@ -106,6 +94,8 @@ function drawBall(){
             break;
         }
  }
+ document.addEventListener('keydown', moveUser)
+
 
   // drawUser
   function drawUser(){
@@ -113,40 +103,65 @@ function drawBall(){
     user.style.bottom = currentPosition[1] + 'px'
  }
 
-
+// draw ball
+function drawBall(){
+    ball.style.left = ballCurrentPosition[0] + 'px'
+    ball.style.bottom = ballCurrentPosition[1] + 'px'
+}
 
  // move ball 
  function moveBall(){
-    ballCurrentPosition[0] +=2
-    ballCurrentPosition[1] +=2
+    ballCurrentPosition[0] += xDirection
+    ballCurrentPosition[1] += yDirection
     drawBall()
+    checkForCollisions()
  }
 
  timerId = setInterval(moveBall, 30)
 
  // check for collisions
  function checkForCollisions() {
-// check for block collisions
-for (let i = 0; i < blocks.length; i++){
-    if(
-        (ballCurrentPosition[0] > blocks[i].bottomLeft[0] && ballCurrentPosition[0] < blocks[i].bottomRight[0]) && ((ballCurrentPosition[1] + ballDiameter) > blocks[i].bottomLeft[1] && ballCurrentPosition[1] < blocks[i].topLeft[1]){
-        
-        }
-    )
-}
+    //check for block collision
+    for (let i = 0; i < blocks.length; i++){
+      if
+      (
+        (ballCurrentPosition[0] > blocks[i].bottomLeft[0] && ballCurrentPosition[0] < blocks[i].bottomRight[0]) &&
+        ((ballCurrentPosition[1] + ballDiameter) > blocks[i].bottomLeft[1] && ballCurrentPosition[1] < blocks[i].topLeft[1]) 
+      )
+        {
+        const allBlocks = Array.from(document.querySelectorAll('.block'))
+        allBlocks[i].classList.remove('block')
+        blocks.splice(i,1)
+        changeDirection()   
+        score++
+        scoreDisplay.innerHTML = score
+        if (blocks.length == 0) {
+          scoreDisplay.innerHTML = 'You Win!'
+          clearInterval(timerId)
+          document.removeEventListener('keydown', moveUser);
 
-    // check for wall collisions
-    if(ballCurrentPosition[0] >= (boardWidth - ballDiameter) || ballCurrentPosition[1] >= (boardHeight - ballDiameter) || ballCurrentPosition[0] <= 0 ) {
-        changeDirection()
+        }
+      }
     }
 
+    // check for wall collisions
+    if (ballCurrentPosition[0] >= (boardWidth - ballDiameter) || ballCurrentPosition[0] <= 0 || ballCurrentPosition[1] >= (boardHeight - ballDiameter))
+    {
+      changeDirection()
+    }
+  
 // check for user collisions
-if(ballCurrentPosition[0] > currentPosition[0] && ballCurrentPosition[0] < currentPosition[0] + blockWidth) && (ball)
-
+if(
+    (ballCurrentPosition[0] > currentPosition[0] && ballCurrentPosition[0] < currentPosition[0] + blockWidth) && (ballCurrentPosition[1] > currentPosition[1] && ballCurrentPosition[1] < currentPosition[1] + blockHeight)
+)
+{
+    changeDirection()
+}
     //check for game over 
     if (ballCurrentPosition[1] <= 0) {
         clearInterval(timerId)
-        scoreDisplay.innerHtml = 'You Lose!'
+        scoreDisplay.innerHTML = 'You Lose!'
+        document.removeEventListener('keydown', moveUser)
     }
  }
 
@@ -160,7 +175,7 @@ if(ballCurrentPosition[0] > currentPosition[0] && ballCurrentPosition[0] < curre
         return 
     }
     if( xDirection === -2 && yDirection === -2){
-        xDirection  = 2
+        yDirection  = 2
         return
     }
     if( xDirection === -2 && yDirection === 2){
